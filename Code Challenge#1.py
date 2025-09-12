fdd8a385d79032335e3fea39d2a7a89d30e8708e
John Axel De Leon <cedrickcontreras100@gmail.com>
2025-09-12 14:23:30 +0800

    Rename Code Challenge#1.py to codechallenge#1.py

--- Code Challenge#1.py
+++
@@ -1,2 +0,0 @@
-r = input("Halloww! Name Please: ")
-print("\t\t\t\t\t*\t\t\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t*\t\t\tHi!\t\t\t*\t\n\t*\t\t\t\t",r,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t\t\t*\t\t\t\t")
---
+++ codechallenge#1.py
@@ -0,0 +1,2 @@
+r = input("Halloww! Name Please: ")
+print("\t\t\t\t\t*\t\t\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t*\t\t\tHi!\t\t\t*\t\n\t*\t\t\t\t",r,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\t\n\t\t\t*\t\t\t\t*\t\t\n\t\t\t\t*\t\t*\t\t\t\n\t\t\t\t\t*\t\t\t\t")
