5eaf808e145201185f9fba89c1cf35049ef8fa73
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2025-11-15 12:36:29 +0800

    Dictionary

--- Activity26.py
+++ Activity26.py
@@ -1,3 +1,4 @@
-prog = {'easy':'py','easy2':'css'}
+prog = {'easy':'py','easy2':'css','easy3':'html'} #Dictionary
 
-print(prog['easy2'])
+
+print(prog['easy2']) #Printing the key of the item in the dictionary
